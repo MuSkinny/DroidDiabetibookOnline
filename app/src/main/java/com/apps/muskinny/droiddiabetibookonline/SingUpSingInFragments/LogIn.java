73f81a6554b08659834f48a34b88333f0e4da5fb
Marco Falanga <marco.fala97@gmail.com>
2018-05-21 15:48:10 +0200

    added auth, styles, some layout

--- app/src/main/java/com/apps/muskinny/droiddiabetibookonline/SingUpSingInFragments/LogIn.java
+++ app/src/main/java/com/apps/muskinny/droiddiabetibookonline/SingUpSingInFragments/LogIn.java
@@ -7,10 +7,20 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.apps.muskinny.droiddiabetibookonline.R;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class LogIn extends Fragment {
+public class LogIn extends Fragment implements View.OnClickListener
+{
+    private FirebaseAuth logInAuth;
+    private Button LogInBtn;
+    private EditText lEmailEdtxt, lPasswordEdtxt;
+    private View view;
+    private String lEmail, lPassword;
 
     static final String TAG = "LogIn Fragment";
 
@@ -20,15 +30,40 @@ public class LogIn extends Fragment {
 
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+
+        logInAuth = FirebaseAuth.getInstance();
+        if (logInAuth.getCurrentUser() != null)
+        {
+            //open UserActivity
+            showToast("Logged in");
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.log_in_fragment, container, false);
+        view = inflater.inflate(R.layout.log_in_fragment, container, false);
+
+        LogInBtn = (Button) view.findViewById(R.id.logIn_button);
+        lEmailEdtxt = (EditText) view.findViewById(R.id.logIn_email);
+        lPasswordEdtxt = (EditText) view.findViewById(R.id.logIn_password);
+
+        LogInBtn.setOnClickListener(this);
+
+        return view;
     }
 
+    public void showToast(String message)
+    {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
--- app/src/main/java/com/apps/muskinny/droiddiabetibookonline/SingUpSingInFragments/SingUp.java
+++ app/src/main/java/com/apps/muskinny/droiddiabetibookonline/SingUpSingInFragments/SingUp.java
@@ -3,17 +3,19 @@ package com.apps.muskinny.droiddiabetibookonline.SingUpSingInFragments;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EdgeEffect;
 import android.widget.EditText;
-
+import android.widget.Toast;
 import com.apps.muskinny.droiddiabetibookonline.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SingUp extends Fragment implements View.OnClickListener
@@ -21,8 +23,8 @@ public class SingUp extends Fragment implements View.OnClickListener
     private FirebaseAuth registerAuth;
     private Button registerBtn;
     private EditText rEmailEdtxt, rPasswordEdtxt, rNameEdtxt;
-
-
+    private View view;
+    private String rName, rEmail, rPassword;
 
 
     public SingUp()
@@ -39,6 +41,7 @@ public class SingUp extends Fragment implements View.OnClickListener
         if (registerAuth.getCurrentUser() != null)
         {
             //open UserActivity
+            showToast("Logged in");
         }
 
     }
@@ -47,7 +50,16 @@ public class SingUp extends Fragment implements View.OnClickListener
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.sing_up_fragment, container, false);
+        view =  inflater.inflate(R.layout.sing_up_fragment, container, false);
+
+        registerBtn = (Button) view.findViewById(R.id.register_button);
+        rEmailEdtxt = (EditText) view.findViewById(R.id.r_email);
+        rPasswordEdtxt = (EditText) view.findViewById(R.id.r_password);
+        rNameEdtxt = (EditText) view.findViewById(R.id.r_name);
+
+        registerBtn.setOnClickListener(this);
+
+        return view;
     }
 
     public void loadFragment(Fragment fragment)
@@ -58,10 +70,82 @@ public class SingUp extends Fragment implements View.OnClickListener
         fragmentTransaction.commit();
     }
 
+    public void SingUp()
+    {
+        if(checkParameters())
+        {
+            registerAuth.createUserWithEmailAndPassword(rEmail, rPassword)
+                    .addOnCompleteListener(this.getActivity(), new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task)
+                        {
+                            if(task.isSuccessful())
+                            {
+                                showToast("Registration Completed!");
+                            }
+                            else if (!task.isSuccessful())
+                            {
+                                showToast("Registration failed");
+                                try {
+                                    throw task.getException();
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                        }
+                    });
+        }
+
+    }
+
+    public boolean checkParameters()
+    {
+       rName = rNameEdtxt.getText().toString();
+       rEmail = rEmailEdtxt.getText().toString().trim();
+       rPassword = rPasswordEdtxt.getText().toString().trim();
+
+        if (TextUtils.isEmpty(rEmail)) {
+            showToast("Enter email address!");
+
+            return false;
+        }
+
+        if (TextUtils.isEmpty(rPassword)) {
+            showToast("Enter password!");
+
+            return false;
+        }
+
+        if (rPassword.length() < 8) {
+            showToast("Password too short, enter minimum 8 characters!");
+
+            return false;
+
+        }
+
+        if (TextUtils.isEmpty(rName)) {
+            showToast("Enter your name!");
+
+            return false;
+        }
+
+        return true;
+    }
+
 
     @Override
     public void onClick(View v)
     {
+        if (v == registerBtn)
+        {
+            SingUp();
+        }
+    }
 
+    public void showToast(String message)
+    {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
     }
 }
+
