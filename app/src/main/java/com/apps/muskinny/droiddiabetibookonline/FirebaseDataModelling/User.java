ceb5ccf90a9bd5704a2bb733cdd9a27c04ab9088
MuSkinny <marco.fala97@gmail.com>
2018-05-26 19:33:52 +0200

    started adding som data modelling

---
+++ app/src/main/java/com/apps/muskinny/droiddiabetibookonline/FirebaseDataModelling/User.java
@@ -0,0 +1,26 @@
+package com.apps.muskinny.droiddiabetibookonline.FirebaseDataModelling;
+
+public class User {
+    final static String USERS = "users";
+
+    private String email;
+    private String userID;
+
+    public User() {
+    }
+
+    public User(String userID, String email) {
+        this.userID = userID;
+        this.email = email;
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+}
